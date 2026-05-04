385d0a507d67f61a13b7cc60a5e2e133d550f071
Thanh Hoang <henry.nguyen073199@gmail.com>
2026-05-04 17:45:00 +0700

    feat(problem2): currency swap form with searchable token dropdown

--- src/problem2/src/main.tsx
+++ src/problem2/src/main.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SwapForm } from './swap-form';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <main className="app">
-      <div className="card">
-        <h1>Swap</h1>
-        <p>Coming soon.</p>
-      </div>
+      <SwapForm />
     </main>
   </React.StrictMode>,
 );
